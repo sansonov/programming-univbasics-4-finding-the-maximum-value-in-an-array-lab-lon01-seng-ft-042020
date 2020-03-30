244eabc651fa4a21383cd234f704741c5b4b2a07
Adao Natalino <sansonov@gmail.com>
2020-03-30 21:58:19 +0000

    Done.

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,13 @@
+require 'pry'
 def find_max_value(array)
-  # Add your solution here
+  i = 0
+  a = -10000000000
+  while i < array.length do
+    if a < array[i]
+      a = array[i]
+    end
+    i+=1
+    #binding.pry
+  end
+  return a.to_i
 end
